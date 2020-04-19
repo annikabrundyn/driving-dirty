e2a720e3ace4c48ee76098e1c65fd34a53ae268f
Noah Kasmanoff <nkasmanoff@gmail.com>
2020-04-19 11:26:10 -0400

    commenting out gpu request ... for now ...

--- model_training.sh
+++ model_training.sh
@@ -4,7 +4,7 @@
 #SBATCH --cpus-per-task=12
 #SBATCH --time=4:00:00
 #SBATCH --mem=25GB
-#SBATCH --gres=gpu:1
+##SBATCH --gres=gpu:1
 #SBATCH --job-name=roadmap_baseline
 #SBATCH --mail-type=END
 ##SBATCH --mail-user=nsk367@nyu.edu
