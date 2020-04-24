5bdbf0eb7f207d079d16665da83752f588496171
Annika Brundyn <annikabrundyn1@gmail.com>
2020-04-24 08:32:55 -0400

    added bounding box script (empty)

---
+++ src/bounding_box_model/bb_MLP.py
@@ -0,0 +1,6 @@
+'''
+This script should contain the model that takes the images, converts the bounding box output to images,
+does feature extraction, tries to predict bounding box image and then converts the image back to coordinate
+predictions.
+'''
+
