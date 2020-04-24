f8d38700df1849fe2351a08f303bf085083fd339
Annika Brundyn <annikabrundyn1@gmail.com>
2020-04-24 11:05:14 -0400

    commit

--- src/autoencoder/autoencoder.py
+++ src/autoencoder/autoencoder.py
@@ -73,6 +73,8 @@ class BasicAE(LightningModule):
         # (batch, imgs, c, h=256, w=306)
         #input = torch.randn(self.batch_size, 6, self.in_channels, 256, 306)
 
+        import pdb; pdb.set_trace()
+
         x, y = self.six_to_one_task(batch)
 
         # Encode - z has dim batch_size x latent_dim
