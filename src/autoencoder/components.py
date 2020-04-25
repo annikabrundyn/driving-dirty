52a4efcaafcc80f8470d50aa725a6b85b57591b5
Annika Brundyn <annikabrundyn1@gmail.com>
2020-04-25 16:28:03 -0400

    merge

--- src/autoencoder/components.py
+++ src/autoencoder/components.py
@@ -40,7 +40,7 @@ class Encoder(torch.nn.Module):
         x = F.relu(self.c2(x))
         x = F.relu(self.c3(x))
         x = x.view(x.size(0), -1).unsqueeze(1)
-        x = F.max_pool1d(x, kernel_size=self.pooling_size).squeeze(0)
+        x = F.max_pool1d(x, kernel_size=self.pooling_size).squeeze(1)
         x = self.fc1(x)
         x = self.fc2(x)
 
