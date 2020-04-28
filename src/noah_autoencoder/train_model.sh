d41439c008a69bd1b26eaff102a73de1565a32d8
nkasmanoff <nkasmanoff@gmail.com>
2020-04-28 00:16:17 -0400

    my model

--- src/noah_autoencoder/train_model.sh
+++ src/noah_autoencoder/train_model.sh
@@ -4,7 +4,7 @@
 #SBATCH --cpus-per-task=12
 #SBATCH --time=24:00:00
 #SBATCH --mem=8GB
-#SBATCH --gres=gpu:p40:1
+#SBATCH --gres=gpu:1
 #SBATCH --job-name=Train_Inception
 #SBATCH --mail-type=END
 #SBATCH --output=slurm_%j.out
