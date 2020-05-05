1401d53acd8a9d820e8d239f9c67f47733f720e8
Noah Kasmanoff <nkasmanoff@gmail.com>
2020-05-04 21:17:07 -0400

    looping is funny

--- src/noah_data/sketch_regions.py
+++ src/noah_data/sketch_regions.py
@@ -37,8 +37,8 @@ def sketch_regions(target):
     """
 
 
-    sketched_grids = []
-    for t in target: #iterate over batch
+    sketch_grids = []
+    for t in range(len(target)): #iterate over batch
 
         # The ego car position terat
         grid = torch.zeros([800,800])
@@ -57,15 +57,42 @@ def sketch_regions(target):
                 grid_[xi,yi] = 1.
             x, y = torch.where(grid_ > 0)
             j = 0
-            while j < 100:
+            while j < 128:
                 a,b = random.sample(range(x.min(),x.max()),2)
                 c , d = random.sample(range(y.min(),y.max()),2)
-                rr, cc = line(a, c, b, d) #
+                rr, cc = line(a, c, b-1, d-1) #
                 grid[rr, cc] = 1.
                 j +=1
 
-        sketched_grids.append(grid)
+        sketch_grids.append(grid)
 
     return torch.stack(sketch_grids)
 
 
+
+if __name__ == '__main__':
+    import matplotlib.pyplot as plt
+
+    image_folder = '/Users/noahkasmanoff/Desktop/Deep_Learning/car/dat/data'
+    annotation_file = image_folder + '/annotation.csv'
+    batch_size = 4
+
+
+    labeled_scene_index = np.arange(106, 134)
+    transform = torchvision.transforms.ToTensor()
+    labeled_trainset = LabeledDataset(image_folder=image_folder,
+    annotation_file=annotation_file,
+    scene_index=labeled_scene_index,
+    transform=transform,
+    extra_info=True
+    ) 
+
+    trainloader = torch.utils.data.DataLoader(labeled_trainset, batch_size=4,shuffle=True, num_workers=2, collate_fn=collate_fn)
+    sample, target, road_image, extra = iter(trainloader).next()
+
+    print("Now running a test ...")
+
+    target_image = sketch_regions(target)
+
+    plt.imshow(target_image[2],cmap='gray')
+    plt.show()
