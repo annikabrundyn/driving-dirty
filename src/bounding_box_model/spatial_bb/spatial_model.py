2d294d3d52c74b98facdc1bf5b393350ecd540b5
a <annikabrundyn1@gmail.com>
2020-05-06 00:50:10 -0400

    update spatial model

--- src/bounding_box_model/spatial_bb/spatial_model.py
+++ src/bounding_box_model/spatial_bb/spatial_model.py
@@ -203,12 +203,12 @@ class BBSpatialModel(LightningModule):
 
         # want to optimize this parameter
         #parser.opt_list('--batch_size', type=int, default=16, options=[16, 10, 8], tunable=False)
-        parser.opt_list('--learning_rate', type=float, default=0.005, options=[1e-2, 1e-3, 1e-4, 1e-5], tunable=True)
+        parser.opt_list('--learning_rate', type=float, default=0.001, options=[1e-2, 1e-3, 1e-4, 1e-5], tunable=True)
         parser.add_argument('--batch_size', type=int, default=32)
         # fixed arguments
-        parser.add_argument('--link', type=str, default='/Users/annika/Developer/driving-dirty/data')
-        parser.add_argument('--pretrained_path', type=str, default='/Users/annika/Developer/driving-dirty/lightning_logs/version_3/checkpoints/epoch=4.ckpt')
-        parser.add_argument('--output_img_freq', type=int, default=1000)
+        parser.add_argument('--link', type=str, default='/scratch/ab8690/DLSP20Dataset/data')
+        parser.add_argument('--pretrained_path', type=str, default='/scratch/ab8690/logs/dd_pretrain_ae/lightning_logs/version_9234267/checkpoints/epoch=42.ckpt')
+        parser.add_argument('--output_img_freq', type=int, default=500)
         return parser
 
 
