2be92f8f46221e995a3ee42a23124942d5c0e089
a <annikabrundyn1@gmail.com>
2020-05-08 02:26:30 -0400

    c

--- src/bounding_box_model/fast_rcnn/bb_fast_rcnn_w_map.py
+++ src/bounding_box_model/fast_rcnn/bb_fast_rcnn_w_map.py
@@ -292,7 +292,7 @@ class FasterRCNNRoadMap(LightningModule):
         # want to optimize this parameter
         #parser.opt_list('--batch_size', type=int, default=16, options=[16, 10, 8], tunable=False)
         parser.opt_list('--learning_rate', type=float, default=0.001, options=[1e-3, 1e-4, 1e-5], tunable=True)
-        parser.add_argument('--batch_size', type=int, default=10)
+        parser.add_argument('--batch_size', type=int, default=8)
         # fixed arguments
         parser.add_argument('--link', type=str, default='/scratch/ab8690/DLSP20Dataset/data')
         parser.add_argument('--pretrained_path', type=str, default='/scratch/ab8690/logs/space_bb_pretrain/lightning_logs/version_9604234/checkpoints/epoch=23.ckpt')
--- src/submit_bb.py
+++ src/submit_bb.py
@@ -80,7 +80,7 @@ if __name__ == '__main__':
 
     parser = HyperOptArgumentParser(add_help=False, strategy='grid_search')
     parser = Trainer.add_argparse_args(parser)
-    parser.add_argument('--model', type=str, default='faster_rcnn')
+    parser.add_argument('--model', type=str, default='faster_rcnn_rm')
 
     (temp_args, arr) = parser.parse_known_args()
     model_name = temp_args.model
@@ -92,7 +92,7 @@ if __name__ == '__main__':
     parser.add_argument('--nodes', type=int, default=1)
     parser.add_argument('--conda_env', type=str, default='driving-dirty')
     parser.add_argument('--on_cluster', default=True, action='store_true')
-    parser.add_argument('-n', '--tt_name', default='frcnn2_no_rm')
+    parser.add_argument('-n', '--tt_name', default='frcnn2_w_rm')
     parser.add_argument('-d', '--tt_description', default='pretrained ae for feature extraction')
     parser.add_argument('--logs_save_path', default='/scratch/ab8690/logs')
     parser.add_argument('--single_run', dest='single_run', action='store_true')
