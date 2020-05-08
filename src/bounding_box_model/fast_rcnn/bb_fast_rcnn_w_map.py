5de2c033fab34b768dd04c92b858363498c2cc34
a <annikabrundyn1@gmail.com>
2020-05-08 19:35:29 -0400

    j

--- src/bounding_box_model/fast_rcnn/bb_fast_rcnn_w_map.py
+++ src/bounding_box_model/fast_rcnn/bb_fast_rcnn_w_map.py
@@ -230,7 +230,11 @@ class FasterRCNNRoadMap(LightningModule):
 
         # output dim: [N, 4] where each box has [x1, x2, x3, x4]
 #        coords = torch.stack([min_x, max_y, max_x, min_y], dim=1)
-        coords = torch.stack([10, 10, 20, 20], dim=1)
+        min_x[:] = 0
+        max_y[:] = 0
+        max_x[:] = 0
+        min_y[:] = 0
+        coords = torch.stack([min_x, max_y, max_x, min_y], dim=1)
 
         return coords
 
